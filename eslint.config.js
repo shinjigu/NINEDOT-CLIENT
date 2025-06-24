576d3aeeea028026497082e6ecde72d087ea790b
Saebom <spring180@naver.com>
2025-06-24 20:58:54 +0900

    chore: eslint.config.js 파일 생성

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,58 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintPluginImport from 'eslint-plugin-import';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+export default tseslint.config({
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    globals: globals.browser,
+  },
+  plugins: {
+    react,
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+    import: eslintPluginImport,
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
   },
-)
+  ignores: ['node_modules', 'dist', 'dist-ssr'],
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    'eslint-config-prettier',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-console': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    curly: 'error',
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          ['parent', 'sibling', 'index'],
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    'react/self-closing-comp': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+});
