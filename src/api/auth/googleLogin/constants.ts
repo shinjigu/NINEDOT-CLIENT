d69317fd76f5a70c2245475cdb7acc05aa39a19a
Saebom <spring180@naver.com>
2025-06-29 23:09:50 +0900

    fix: response type을 code로 변경

--- src/api/auth/googleLogin/constants.ts
+++ src/api/auth/googleLogin/constants.ts
@@ -2,4 +2,4 @@ export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 export const GOOGLE_REDIRECT_URI = import.meta.env.VITE_GOOGLE_REDIRECT_URI_LOCAL;
 export const GOOGLE_AUTH_BASE_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
 export const GOOGLE_SCOPE = 'openid email profile';
-export const GOOGLE_RESPONSE_TYPE = 'token';
+export const GOOGLE_RESPONSE_TYPE = 'code';
