c120fffe4febcff24795a8bd9c61652375039f79
Saebom <spring180@naver.com>
2025-06-29 22:39:24 +0900

    feat: 로그인 url 생성 함수 제작

---
+++ src/api/auth/googleLogin/utils.ts
@@ -0,0 +1,12 @@
+import {
+  GOOGLE_AUTH_BASE_URL,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_REDIRECT_URI,
+  GOOGLE_SCOPE,
+  GOOGLE_RESPONSE_TYPE,
+} from './constants';
+
+export const generateGoogleLoginUrl = () => {
+  const url = `${GOOGLE_AUTH_BASE_URL}?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=${GOOGLE_RESPONSE_TYPE}&scope=${GOOGLE_SCOPE}`;
+  return url;
+};
