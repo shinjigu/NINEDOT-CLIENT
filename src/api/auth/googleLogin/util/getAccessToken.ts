9f540393369e81d441d33e25d48d5ac00a11ee50
Saebom <spring180@naver.com>
2025-07-02 17:25:48 +0900

    feat: 리디렉트 페이지 작성 예시

--- src/api/auth/googleLogin/util/getAccessToken.ts
+++ src/api/auth/googleLogin/util/getAccessToken.ts
@@ -1,6 +1,8 @@
 import axiosInstance from '../../../axiosInstance';
 
-export const getAccessToken = async (code: string) => {
+const getAccessToken = async (code: string) => {
   const response = await axiosInstance.get(`/api/v1/auth/oauth2/google/callback?code=${code}`);
   return response.data;
 };
+
+export default getAccessToken;
