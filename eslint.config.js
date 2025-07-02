fe76599c7d446094e7c19fb9a4b75ad26606d781
이윤지 <rhaxlddbswl3@gmail.com>
2025-07-02 19:29:16 +0900

    init: 스토리북 설치

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import globals from 'globals';
 import react from 'eslint-plugin-react';
@@ -56,4 +59,4 @@ export default tseslint.config({
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'prettier/prettier': 'error',
   },
-});
+}, storybook.configs["flat/recommended"]);
