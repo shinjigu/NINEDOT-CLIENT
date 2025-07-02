08dc19a358ec58d22af12a6da989731d3ac3c59e
Saebom <spring180@naver.com>
2025-07-03 01:55:49 +0900

    style: 프리텐다드 폰트 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
+import './style/global.css.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/style/global.css.ts
@@ -0,0 +1,8 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+globalStyle('body', {
+  fontFamily: "'Pretendard', sans-serif",
+  margin: 0,
+  padding: 0,
+  backgroundColor: '#fff',
+});
