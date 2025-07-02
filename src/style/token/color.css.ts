2259ecd8331c135943cee6c81cb1e2749f5dbbe7
Saebom <spring180@naver.com>
2025-07-03 01:36:35 +0900

    chore: 색상 토큰 설정

---
+++ src/style/token/color.css.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  black01: '#121212',
+  white01: '#ffffff',
+  bg_black01: '#02050B',
+
+  blue01: '#C3D9FF',
+  blue02: '#8AB4FF',
+  blue03: '#74A6FF',
+  blue04: '#508FFF',
+  blue05: '#3E72F3',
+  blue06: '#2F53C4',
+  blue07: '#1B49D7',
+
+  mint01: '#DEFFEF',
+  mint02: '#A9FFD6',
+  mint03: '#77FFBD',
+  mint04: '#3BFFA0',
+};
