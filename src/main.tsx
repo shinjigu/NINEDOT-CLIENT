9acd6b705671b038e321a95bcdf84c6d37ecc803
jisusin <sjsz0811@hanyang.ac.kr>
2025-07-03 20:10:09 +0900

    refactor: queryClient 설정 별도 파일로 분리

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retryOnMount: true,
-      refetchOnReconnect: false,
-      retry: 2,
-    },
-    mutations: {
-      retry: 1,
-    },
-  },
-});
+import { queryClient } from "./common/util/queryClient.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
