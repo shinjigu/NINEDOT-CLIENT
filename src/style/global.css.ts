e6d8da56e8ba539d4a446f182ea5e288bbcbe03e
Saebom <spring180@naver.com>
2025-07-03 13:11:01 +0900

    init: reset 파일 추가

--- src/style/global.css.ts
+++ src/style/global.css.ts
@@ -1,8 +1,8 @@
 import { globalStyle } from '@vanilla-extract/css';
 
+import './reset.css.ts';
+
 globalStyle('body', {
   fontFamily: "'Pretendard', sans-serif",
-  margin: 0,
-  padding: 0,
   backgroundColor: '#fff',
 });
