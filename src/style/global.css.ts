fb9bedc11217523ba80c204db71c4e33d0d3bb06
Saebom <spring180@naver.com>
2025-07-03 13:16:47 +0900

    chore: 전역 스타일에 속성 추가

--- src/style/global.css.ts
+++ src/style/global.css.ts
@@ -2,7 +2,13 @@ import { globalStyle } from '@vanilla-extract/css';
 
 import './reset.css.ts';
 
+globalStyle('html', {
+  fontSize: '62.5%',
+});
+
 globalStyle('body', {
   fontFamily: "'Pretendard', sans-serif",
   backgroundColor: '#fff',
+  minHeight: '100vh',
+  overflowX: 'hidden',
 });
