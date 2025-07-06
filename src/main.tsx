5e86827316d7be83ac4aab7fb8fe6916edcf6521
shinjigu <sjsz0811@hanyang.ac.kr>
2025-07-06 10:53:19 +0000

    🚀 Latest deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import App from './App.tsx';
 
 import './style/global.css.ts';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { queryClient } from './common/util/queryClient.ts';
 
--- src/style/token/color.css.ts
+++ src/style/token/color.css.ts
@@ -1,7 +1,22 @@
 export const colors = {
   black01: '#121212',
+  black01_70: 'rgba(18, 18, 18, 0.70)',
   white01: '#ffffff',
   bg_black01: '#02050B',
+
+  grey1: '#17191C',
+  grey2: '#1E2127',
+  grey3: '#282C33',
+  grey4: '#2E323A',
+  grey5: '#41454C',
+  grey6: '#5A5E66',
+  grey7: '#999DA4',
+  grey8: '#B1B6BE)',
+  grey9: '#C5C8CE',
+  grey10: '#E3E4E5',
+  grey11: '#FDFDFD',
+  grey11_10: 'rgba(253, 253, 253, 0.10)',
+  grey05_32: 'rgba(65, 69, 76, 0.32)',
 
   blue01: '#C3D9FF',
   blue02: '#8AB4FF',
@@ -15,4 +30,7 @@ export const colors = {
   mint02: '#A9FFD6',
   mint03: '#77FFBD',
   mint04: '#3BFFA0',
+
+  gradient04: 'linear-gradient(180deg, #305088 0%, #1E3270 100%)',
+  gradient05: 'linear-gradient(180deg, #152C56 0%, #0D193E 100%)',
 };
