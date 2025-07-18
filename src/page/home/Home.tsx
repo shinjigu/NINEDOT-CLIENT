623f2c650093d47ac696c2822dab5e17de87a054
shinjigu <sjsz0811@hanyang.ac.kr>
2025-07-18 13:44:52 +0000

    🚀 Latest deployment

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import { HomeContainer } from '@/page/home/Home.css';
 import { INTRO_MESSAGE } from '@/page/home/constant/scrollSection';
 import { useFadeInOnView } from '@/page/home/hook/useFadeInOnView';
@@ -6,28 +8,25 @@ import ScrollSection from '@/page/home/ScrollSection/ScrollSection';
 import EndSection from '@/page/home/EndSection/EndSection';
 import { fadeSlide } from '@/page/home/style/fadeTransition.css';
 import { useMultipleFadeInOnView } from '@/page/home/hook/useMultipleFadeInOnView';
-import LoginModal from '@/common/component/LoginModal/LoginModal';
-import { useModal } from '@/common/hook/useModal';
 import mandalAnimation from '@/assets/lottie/mandalart.json';
 import aiAnimation from '@/assets/lottie/ai.json';
+import { PATH } from '@/route';
 import todoAnimation from '@/assets/lottie/todo.json';
-
 const animationDataArray = [mandalAnimation, aiAnimation, todoAnimation];
 const sectionKeys = ['mandalart', 'ai', 'todo'] as const;
 
 const Home = () => {
+  const navigate = useNavigate();
   const scrolls = useMultipleFadeInOnView();
   const end = useFadeInOnView<HTMLDivElement>();
 
-  const { openModal, closeModal, ModalWrapper } = useModal();
-
   const handleOpenLogin = () => {
-    openModal(<LoginModal onClose={closeModal} />);
+    // openModal(<LoginModal onClose={closeModal} />);
+    navigate(PATH.TODO);
   };
 
   return (
     <div className={HomeContainer}>
-      {ModalWrapper}
       <StartSection onClick={handleOpenLogin} />
 
       {sectionKeys.map((key, index) => {
