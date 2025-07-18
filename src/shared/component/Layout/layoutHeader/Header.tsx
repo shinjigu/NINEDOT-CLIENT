f318c053385a47fb25acbdf4a13379d28f3972b2
shinjigu <sjsz0811@hanyang.ac.kr>
2025-07-18 19:40:47 +0000

    🚀 Latest deployment

--- src/shared/component/Layout/layoutHeader/Header.tsx
+++ src/shared/component/Layout/layoutHeader/Header.tsx
@@ -1,14 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate, useLocation, Link } from 'react-router-dom';
 
 import * as styles from './Header.css';
 
 import { PATH } from '@/route/path';
 import IcLogo from '@/assets/svg/IcLogo';
-import LoginModal from '@/common/component/LoginModal/LoginModal';
-import UserModal from '@/common/component/UserModal/UserModal';
-import { useModal } from '@/common/hook/useModal';
-import { useGetUser } from '@/api/domain/signup/hook/useGetUser';
 
 const MENUS = [
   { label: '나의 할 일', path: PATH.TODO },
@@ -19,33 +15,15 @@ const MENUS = [
 const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const accessToken = localStorage.getItem('accessToken');
 
   const findActiveMenu = MENUS.find((menu) => location.pathname.startsWith(menu.path));
   const initialMenu = findActiveMenu ? findActiveMenu.label : '';
+
   const [activeMenu, setActiveMenu] = useState<string>(initialMenu);
-
-  const [openProfile, setOpenProfile] = useState<boolean>(false);
-  const { openModal, closeModal, ModalWrapper } = useModal();
-
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(!!accessToken);
-  const { data: user, isLoading } = useGetUser();
-
-  useEffect(() => {
-    setIsLoggedIn(!!localStorage.getItem('accessToken'));
-  }, []);
-
-  const handleLogin = () => {
-    openModal(<LoginModal onClose={closeModal} />);
-  };
 
   const handleMenuClick = (menuLabel: string, path: string) => {
     setActiveMenu(menuLabel);
     navigate(path);
-  };
-
-  const handleProfile = () => {
-    setOpenProfile((prev) => !prev);
   };
 
   const renderNavMenu = () => (
@@ -67,18 +45,6 @@ const Header = () => {
           );
         })}
       </nav>
-
-      {!isLoading && user && (
-        <>
-          <img
-            src={user.profileImageUrl}
-            alt="유저 프로필"
-            className={styles.profilePlaceholder}
-            onClick={handleProfile}
-          />
-          {openProfile && <UserModal onClose={handleProfile} />}
-        </>
-      )}
     </>
   );
 
@@ -89,17 +55,9 @@ const Header = () => {
           <IcLogo className={styles.logoImage} />
         </Link>
 
-        {isLoggedIn ? (
-          renderNavMenu()
-        ) : (
-          <button className={styles.loginButton} onClick={handleLogin}>
-            로그인
-          </button>
-        )}
+        {renderNavMenu()}
       </div>
-      {ModalWrapper}
     </header>
   );
 };
-
 export default Header;
