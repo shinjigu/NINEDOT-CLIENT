ad17dc9456ca5fc8e19f10702cd450d913d62ec5
shinjigu <sjsz0811@hanyang.ac.kr>
2025-07-18 18:40:44 +0000

    🚀 Latest deployment

--- src/api/auth/googleLogin/util/getAccessToken.ts
+++ src/api/auth/googleLogin/util/getAccessToken.ts
@@ -1,10 +1,12 @@
 import axiosInstance from '@/api/axiosInstance';
 
 const getAccessToken = async (code: string) => {
-  // const redirectUri = import.meta.env.VITE_GOOGLE_REDIRECT_URI_LOCAL;
+  const redirectUri = import.meta.env.VITE_GOOGLE_REDIRECT_URI;
 
-  const response = await axiosInstance.post(`/auth/oauth2/google/callback`, { code });
-
+  const response = await axiosInstance.post(
+    `/auth/oauth2/google/callback?redirect_uri=${redirectUri}`,
+    { code },
+  );
   return response.data.data;
 };
 
